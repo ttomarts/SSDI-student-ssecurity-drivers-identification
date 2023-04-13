e35cd2995b9f28edc1c38ab835d6c5e74e068e62
ttomarts <114492429+ttomarts@users.noreply.github.com>
2023-04-13 12:15:14 +0800

    Add files via upload

---
+++ DashB/student.mysql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(191) NOT NULL,
+    email VARCHAR(191) NOT NULL,
+    phone VARCHAR(191) NOT NULL,
+    course VARCHAR(191) NOT NULL
+)
