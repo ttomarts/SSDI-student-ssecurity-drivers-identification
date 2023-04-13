1f05099cdbd0fdfc186a30cc7e222f5f7f40c382
ttomarts <114492429+ttomarts@users.noreply.github.com>
2023-04-13 12:25:05 +0800

    Add files via upload

---
+++ capstone1/welcome.php
@@ -0,0 +1,18 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome<?php
+
+
